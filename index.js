e015eb71038e4884c180cfcb934ebdb9b712845b
Alvin Liang <ayliang@gmail.com>
2016-10-10 10:44:09 -0700

    Make it work in pull request review text areas
    Not super future proof, but should work for now. Could also just use `textarea` lol

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ require('bootstrap/js/popover')
 // 1. creating issue
 // 2. creating pr
 // 3. comment issue, comment pr, inline pr comment
-const textareaSelectorGithub = ['[name="issue[body]"]', '[name="pull_request[body]"]', '[name="comment[body]"]']
+const textareaSelectorGithub = ['[name="issue[body]"]', '[name="pull_request[body]"]', '[name="comment[body]"]', '[name="pull_request_review[body]"]']
 const textareaSelectorBitbucket = ['.bb-mention-input.markItUpEditor']
 
 const textareaSelectors = _.join(_.concat(textareaSelectorGithub, textareaSelectorBitbucket))

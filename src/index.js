42f4956c073d0c2a803f5b9ca4d81e1547ab47e5
kevin <me@kevinformatics.com>
2020-07-21 01:21:51 -0700

    Major modernization effort, 2020 edition
    Trying to solve two primary product issues:
    1. Display more than 20 gifs.
    2. Automatic repositioning if popover off screen.
    
    Replaced the following technologies:
    * jQuery with vanilla JS and delegated-events lib
    * Bootstrap with tippy.js
    * webpack with parcel
    * Directly querying the giphy API with @giphy/js-fetch-api
    
    Generally, using these libraries has also reduced the amount of code and
    configuration required to make this work.

---
+++ src/index.js
@@ -0,0 +1,172 @@
+import { GiphyFetch } from "@giphy/js-fetch-api";
+import { on } from "delegated-events";
+import { debounce } from "lodash";
+import randomColor from "randomcolor";
+import tippy from "tippy.js";
+
+import "./styles.css";
+
+const GITHUB_TEXTAREAS = [
+  '[name="issue[body]"]', // create issue
+  '[name="pull_request[body]"]', // create PR
+  '[name="comment[body]"]', // create issue/PR comment
+  '[name="pull_request_review[body]"]', // create PR review comment dropdown
+  '[name="issue_comment[body]"]', // edit issue/PR comment
+].join(", ");
+
+const giphy = new GiphyFetch(process.env.GIPHY_API_KEY);
+const gitphyRegex = /\(gif:([\w\s]+)\)/;
+
+let popover = null;
+let prevQuery = null;
+
+class Popover {
+  constructor(textarea) {
+    const popoverTemplate = `
+      <div class="gitphy--popover">
+        <div class="gitphy--popover-content"></div>
+        <div class="giphy--attribution-wrapper">
+          <img class="giphy--attribution" src="${chrome.extension.getURL(
+            "src/images/giphy_attribution.png"
+          )}">
+        </div>
+      </div>
+    `;
+
+    this._popover = tippy(textarea, {
+      allowHTML: true,
+      content: popoverTemplate,
+      interactive: true,
+      maxWidth: 650,
+      trigger: "manual",
+      theme: "light",
+    });
+
+    this.textarea = textarea;
+  }
+
+  show() {
+    this._popover.show();
+  }
+
+  hide() {
+    this._popover.hide();
+  }
+
+  destroy() {
+    this._popover.destroy();
+  }
+
+  _render(content) {
+    document.querySelector(".gitphy--popover-content").innerHTML = content;
+  }
+
+  renderGifs(gifs) {
+    const gifEls = gifs
+      .map((gif) => {
+        const img = document.createElement("img");
+        img.className = "gitphy--gif";
+        img.style.backgroundColor = randomColor({ luminosity: "bright" });
+        img.setAttribute("src", gif.images.fixed_width.url);
+        img.setAttribute("data-gif-url", gif.images.downsized_medium.url);
+        return img.outerHTML;
+      })
+      .join("");
+    this._render(gifEls);
+  }
+
+  renderLoading(query) {
+    const div = document.createElement("div");
+    div.className = "gitphy--intermediate-state";
+    div.innerHTML = `Loading gifs for "${query}"...`;
+
+    this._render(div.outerHTML);
+  }
+
+  renderNoResults(query) {
+    const div = document.createElement("div");
+    div.className = "gitphy--intermediate-state";
+    div.innerHTML = `No results found for "${query}"...`;
+
+    this._render(div.outerHTML);
+  }
+}
+
+const getQuery = (text) => {
+  const match = text.match(gitphyRegex);
+  if (match) {
+    return match[1].trim();
+  }
+  return null;
+};
+
+const _handleTextareaChange = async (event) => {
+  const textarea = event.target;
+  const query = getQuery(textarea.value);
+
+  if (!popover) {
+    popover = new Popover(textarea);
+  }
+
+  // if the user backspaces into the parens will close the popover
+  query ? popover.show() : popover.hide();
+
+  // if there is a query and it wasn't same as previous one,
+  // search for gifs and load then into the popover
+  if (query && prevQuery !== query) {
+    popover.renderLoading(query);
+
+    const { data: gifs } = await giphy.search(query, { limit: 100 });
+    if (gifs.length === 0) {
+      popover.renderNoResults(query);
+    } else {
+      popover.renderGifs(gifs);
+    }
+
+    prevQuery = query;
+  }
+};
+
+const handleGifClick = (event) => {
+  const gifUrl = event.currentTarget.dataset.gifUrl;
+
+  const { textarea } = popover;
+  textarea.value = textarea.value.replace(gitphyRegex, `(${gifUrl})`);
+};
+
+const handleTextareaBlur = () => {
+  if (popover) {
+    popover.destroy();
+  }
+  popover = null;
+  prevQuery = null;
+};
+
+const handleGifMouseover = (event) => {
+  event.currentTarget.style.borderColor = randomColor({ luminosity: "bright" });
+};
+
+const handleGifMouseout = (event) => {
+  event.currentTarget.style.borderColor = "#fff";
+};
+
+// handle textarea on:
+// * focus (if tabbed in)
+// * input (typed in)
+// * mouseup (on click release)
+const handleTextareaChange = debounce(_handleTextareaChange, 500);
+["focusin", "input", "mouseup"].forEach((eventType) => {
+  on(eventType, GITHUB_TEXTAREAS, (event) => {
+    handleTextareaChange(event);
+  });
+});
+
+// close popover, destroy, and clear last query when textarea is unfocused
+on("focusout", GITHUB_TEXTAREAS, handleTextareaBlur);
+
+// listen on clicks on gifs in popover
+on("mousedown", ".gitphy--gif", handleGifClick);
+
+// add/remove border to/from gif on hover for active state
+on("mouseover", ".gitphy--gif", handleGifMouseover);
+on("mouseout", ".gitphy--gif", handleGifMouseout);

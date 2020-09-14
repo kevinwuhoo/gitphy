8d59a4bd06db4bd82b636d877ad896c7b8257738
Alvin Liang <ayliang@gmail.com>
2020-09-14 02:30:09 -0700

    Make it work in firefox (#20)
    Unfortunately, a long-running issue in Firefox stops us from loading
    the giphy images into img tags:
    https://bugzilla.mozilla.org/show_bug.cgi?id=1267027
    
    * Add a giant hack workaround for CSP problems in firefox
    * Update browserslist for firefox
    * Add CSP and permissions for firefox to manifest
    * Use chrome.runtime instead of deprecated chrome.extension to get giphy-attribution.png url
    * bump version to 1.1.0

--- src/index.js
+++ src/index.js
@@ -22,13 +22,13 @@ let prevQuery = null;
 
 class Popover {
   constructor(textarea) {
+    // Also works for firefox, they emulate the chrome object
+    const giphyAttributionURL = chrome.runtime.getURL("giphy-attribution.png");
     const popoverTemplate = `
       <div class="gitphy--popover">
         <div class="gitphy--popover-content"></div>
         <div class="giphy--attribution-wrapper">
-          <img class="giphy--attribution" src="${chrome.extension.getURL(
-            "giphy-attribution.png"
-          )}">
+          <img class="giphy--attribution" src="${giphyAttributionURL}">
         </div>
       </div>
     `;
@@ -44,6 +44,9 @@ class Popover {
     });
 
     this.textarea = textarea;
+    // TODO: Check if CSP is in effect, that would be better.
+    // Then firefox could be fast on pages without CSP.
+    this.isFirefox = navigator.userAgent.indexOf("Firefox") !== -1;
   }
 
   show() {
@@ -58,18 +61,64 @@ class Popover {
     this._popover.destroy();
   }
 
-  _render(content) {
-    document.querySelector(".gitphy--popover-content").innerHTML = content;
+  _createImgNode(gif) {
+    const img = document.createElement("img");
+    img.className = "gitphy--gif";
+    img.style.backgroundColor = randomColor({ luminosity: "bright" });
+    img.setAttribute("data-gif-url", gif.images.downsized_medium.url);
+    return img;
   }
 
-  renderGifs(gifs) {
+  _getContentNode() {
+    return document.querySelector(".gitphy--popover-content");
+  }
+
+  _render(content) {
+    this._getContentNode().innerHTML = content;
+  }
+
+  async renderGifs(gifs) {
+    if (this.isFirefox) {
+      await this.renderGifsAvoidCSP(gifs);
+    } else {
+      this.renderGifsNoCSP(gifs);
+    }
+  }
+
+  // By using data urls, it works around github's Content-Security-Policy
+  // restrictions, but it loads slower and is more memory-intensive.
+  async renderGifsAvoidCSP(gifs) {
+    // Set up new content node
+    const oldContentNode = this._getContentNode();
+    const newContentNode = oldContentNode.cloneNode(false);
+    // Fill in img tags with no src, but background
+    gifs.forEach((gif) => {
+      const img = this._createImgNode(gif);
+      newContentNode.appendChild(img);
+    });
+    oldContentNode.parentNode.replaceChild(newContentNode, oldContentNode);
+
+    // As images come in, update the img tag to show the image.
+    gifs.forEach(async (gif, idx) => {
+      const r = await fetch(gif.images.fixed_width.url);
+      const b = await r.blob();
+      const dataURL = await new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          resolve(reader.result);
+        };
+        reader.readAsDataURL(b);
+      });
+      const img = newContentNode.childNodes[idx];
+      img.setAttribute("src", dataURL);
+    });
+  }
+
+  renderGifsNoCSP(gifs) {
     const gifEls = gifs
       .map((gif) => {
-        const img = document.createElement("img");
-        img.className = "gitphy--gif";
-        img.style.backgroundColor = randomColor({ luminosity: "bright" });
+        const img = this._createImgNode(gif);
         img.setAttribute("src", gif.images.fixed_width.url);
-        img.setAttribute("data-gif-url", gif.images.downsized_medium.url);
         return img.outerHTML;
       })
       .join("");
@@ -117,11 +166,15 @@ const _handleTextareaChange = async (event) => {
   if (query && prevQuery !== query) {
     popover.renderLoading(query);
 
-    const { data: gifs } = await giphy.search(query, { limit: 100 });
-    if (gifs.length === 0) {
-      popover.renderNoResults(query);
-    } else {
-      popover.renderGifs(gifs);
+    try {
+      const { data: gifs } = await giphy.search(query, { limit: 100 });
+      if (gifs.length === 0) {
+        popover.renderNoResults(query);
+      } else {
+        await popover.renderGifs(gifs);
+      }
+    } catch (err) {
+      throw err;
     }
 
     prevQuery = query;
